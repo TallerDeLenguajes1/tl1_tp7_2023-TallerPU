64f40aa560acf8ceab7d6b72c46367032dc37e68
Sergio Guardia <sguardia@herrera.unt.edu.ar>
2023-06-01 15:58:08 -0300

    V3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+using EspacioCalculadora;
+
+Console.WriteLine("Hello, World!");
+
+Calculadora cal=new Calculadora();
+cal.Suma(15);
+Console.WriteLine(cal.Resultado);
